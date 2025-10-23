be96e81dc54fd2a066cf8e9659052e88c88aa4e2
Ham <jhm010234@naver.com>
2025-10-23 13:50:06 +0900

    Style: S-Core Dream 폰트 테마 시스템 구성

---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+  font: {
+    family: {
+      regular: "'S-CoreDream-4Regular', sans-serif",
+      medium: "'S-CoreDream-5Medium', sans-serif",
+      bold: "'S-CoreDream-6Bold', sans-serif",
+      heavy: "'S-CoreDream-8Heavy', sans-serif",
+    },
+    weight: {
+      thin: 100,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      bold: 600,
+      extraBold: 700,
+      heavy: 800,
+      black: 900,
+    },
+  },
+};
+
+export default theme;
