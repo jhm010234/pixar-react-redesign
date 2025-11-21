30f99cdef92b7f085e056f1d77e9115a9292bbe1
Ham <jhm010234@naver.com>
2025-11-22 01:19:37 +0900

    Feat: MovieList 가운데 정렬

--- src/pages/Films/components/MovieList/MovieList.style.ts
+++ src/pages/Films/components/MovieList/MovieList.style.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex;
+    display: grid;
     padding: 0 100px;
     flex-wrap: wrap;
-    justify-content: space-evenly;
+    justify-items: start;
+    grid-template-columns: repeat(4, 352px);
     row-gap: 72px;
 `;
