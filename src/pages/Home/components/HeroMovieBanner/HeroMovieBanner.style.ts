7a4abd58008266e7897e489b473314f7a8423dce
Ham <jhm010234@naver.com>
2025-12-19 22:29:23 +0900

    Feat: home 페이지 elio banner 제작

--- src/pages/Home/components/HeroMovieBanner/HeroMovieBanner.style.ts
+++ src/pages/Home/components/HeroMovieBanner/HeroMovieBanner.style.ts
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
+export const BannerWrapper = styled.div`
+  position: relative;
+`;
+
 
 export const HeroImg = styled.img`
   width: 100%;
   display: block;
 `;
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  position: absolute;
+  align-items: center;
+  top: 40%;
+  left: 25%;
+  transform: translate(-50%, -50%);
+`;
+
+export const LogoImg = styled.div`
+  display: flex;
+  height: 15.3rem;
+`;
+
+
--- src/pages/Home/components/HeroMovieBanner/index.tsx
+++ src/pages/Home/components/HeroMovieBanner/index.tsx
@@ -1,13 +1,20 @@
 import * as S from "./HeroMovieBanner.style"
 import HeroImg from "@/assets/image/HeroMovieBannerImage.png"
-import { IoPlayOutline } from "react-icons/io5";
+import elioLogo from "@/assets/image/Elio/ElioLogo.png"
+import releaseDate from "@/assets/image/Home/HomeBanner/elio_release_date.png"
+import teaserBtn from "@/assets/image/Home/HomeBanner/elio_teaser_btn.png"
+
 
 
 export default function index() {
   return (
-    <>
-    <S.HeroImg src={HeroImg} alt="Hero Movie Banner" />
-
-    </>
+    <S.BannerWrapper>
+      <S.HeroImg src={HeroImg} alt="Hero Movie Banner" />
+      <S.ImageWrapper>
+        <S.LogoImg><img src={elioLogo} /></S.LogoImg>
+        <div><img src={releaseDate} /></div>
+        <div><img src={teaserBtn} /></div>
+      </S.ImageWrapper>
+    </S.BannerWrapper>
   )
 }
